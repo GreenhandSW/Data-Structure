af7d528f30ba17d604773390c2941fa1f2d0b8ed
swlyrp <ncepu_sw@163.com>
2018-05-24 23:35:11 +0800

    Add 'GenerateRandomString' function which can generate a random string with given length.

--- Library/s_and_r.c
+++ Library/s_and_r.c
@@ -1,6 +1,7 @@
 /* s_and_r.c -- 包含函数rand1()和srand1()的文件 */
 /* 使用ANSI C 的可移植算法 */
 #include<time.h>
+#include "s_and_r.h"
 
 static unsigned long int next = 1;			// 种子
 int rand1(void)
@@ -19,4 +20,15 @@ void srand1(unsigned int seed)
 int Random(int min, int max)
 {
 	return rand1() % (max - min) + min;
+}
+
+void GenerateRandomString(char * random_str, int L)
+{
+	int i;
+	srand1((unsigned)time(0)*rand1());
+	for (i = 0; i < L-1; i++)
+	{
+		random_str[i] = Random(0, 26) + 'a';
+	}
+	random_str[i] = '\0';
 }
--- Library/s_and_r.h
+++ Library/s_and_r.h
@@ -4,3 +4,5 @@
 int rand1(void);
 void srand1(unsigned int seed);
 int Random(int min, int max);
+// Generate a string comprised of random characters with length L
+void GenerateRandomString(char * random_str, int L);
