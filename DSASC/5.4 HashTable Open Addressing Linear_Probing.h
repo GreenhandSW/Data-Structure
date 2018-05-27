68f3503f563a2ffcff630f01054e1dc73cbba865
swlyrp <ncepu_sw@163.com>
2018-05-27 09:13:26 +0800

    Finished the implementation of HashTable with Open Addressing and Quadratic Probing.

---
+++ DSASC/5.4 HashTable Open Addressing Linear_Probing.h
@@ -0,0 +1,34 @@
+#ifndef _HashTable_Open_Addressing_Linear_Probing_H
+#include<string.h>
+#define STRLEN 5
+
+typedef char * ElementType;
+typedef ElementType * Position;
+typedef struct HashTbl HashTbl;
+typedef HashTbl * HashTable;
+typedef ElementType List;
+
+struct HashTbl
+{
+	int TableSize;
+	int CurrentNum;
+	List * Lists;
+};
+
+HashTable InitializeTable(int TableSize);
+// I guess this process makes the Table completely empty,
+// because if I want to make the Table completely empty,
+// it must return NULL, as for 'DestroyTable' doesn't
+// return anything, this process is the only choice.
+HashTable MakeEmpty(HashTable H);
+// I guess this process makes the Lists empty
+void DestroyTable(HashTable H);
+Position Find(ElementType Key, HashTable H);
+void Delete(ElementType Key, HashTable H);
+void Insert(ElementType Key, HashTable H);
+ElementType Retrieve(Position P);
+void Traverse(HashTable H, void(*pfun)(ElementType X));
+int GetSize(HashTable H);
+// get the number of elements in the table
+int GetCurrentCount(HashTable H);
+#endif // !_HashTable_Separate_Chaining_H
---
+++ DSASC/5.4 HashTable Open Addressing Quadratic_Probing.h
@@ -0,0 +1,20 @@
+#ifndef _HashTable_Open_Addressing_Quadratic_Probing_H
+
+#define STRLEN 5
+typedef char * ElementType;
+typedef int Index;
+typedef Index Position;
+
+typedef struct HashTbl HashTbl;
+typedef struct HashTbl * HashTable;
+
+HashTable InitializeTable(int TableSize);
+void DestroyTable(HashTable H);
+void Delete(ElementType Key, HashTable H);
+HashTable MakeEmpty(HashTable H);
+Position Find(ElementType Key, HashTable H);
+void Insert(ElementType Key, HashTable H);
+ElementType Retrieve(Position P, HashTable H);
+void Traverse(HashTable H, void(*pfun)(ElementType X));
+
+#endif // !_HashTable_Open_Addressing_Quadratic_Probing_H
