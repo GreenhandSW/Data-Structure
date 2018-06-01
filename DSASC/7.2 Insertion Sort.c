af493bb2dfffe79613b21a65c39abede4bee311d
swlyrp <ncepu_sw@163.com>
2018-06-01 17:22:37 +0800

    Modified a sentence of comparision, now it's more adaptable.\nBut note that macro definition 'Compare; mustn't be used if there are increment operands in the arguments of it.

--- DSASC/7.2 Insertion Sort.c
+++ DSASC/7.2 Insertion Sort.c
@@ -1,4 +1,6 @@
 #include "7.2 Insertion Sort.h"
+#include"../Library/General.h"
+#define Compare(X, Y) (((X) > (Y)) ? (BIGGER) : (((X) == (Y)) ? (EQUAL) : (SMALLER)))
 
 void InsertionSort(ElementType List[], int Length)
 {
@@ -8,7 +10,7 @@ void InsertionSort(ElementType List[], int Length)
 	for (int i = 1; i < Length; i++)
 	{
 		Tmp = List[i];
-		for (j = i; j > 0 && List[j - 1] > Tmp; j--)
+		for (j = i; j > 0 && Compare(List[j - 1], Tmp) == BIGGER; j--)
 			List[j] = List[j - 1];
 		List[j] = Tmp;
 	}
